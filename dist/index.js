a2f6f7b83f48c63dc067fc335a85da3902f9b8a1
anishp264 <punnenanish@gmail.com>
2023-06-13 13:59:16 +0200

    add SHA for PR

--- dist/index.js
+++ dist/index.js
@@ -10847,6 +10847,8 @@ async function fetchCommitNotesV1(owner, repo, pullRequestNumber){
         container.commitType = prText;
         if(isStringInputValid(commit.commit.message)){
           container.message = commit.commit.message.split("-pr\n\n")[1];
+          container.message += `
+          ${commit.sha}`;
         }        
       }
       else{
